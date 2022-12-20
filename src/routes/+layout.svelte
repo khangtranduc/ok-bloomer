df5088573e5522098745cd0551ade8212bcd39fc
Tran Duc Khang <79127891+khangtranduc@users.noreply.github.com>
2022-12-20 11:50:25 +0800

    Added search bar

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -12,7 +12,17 @@
   <ul>
     <li><a href="/">Home</a></li>
     <li><a href="/about">About</a></li>
-    <li><a href="/contact" role="button">Contact</a></li>
+    <li><a href="/contact">Contact</a></li>
+  </ul>
+  <ul>
+    <li>
+      <form>
+        <input type="search" id="search" name="search" placeholder="Search">
+        <button>
+          <span class="gradient">Go</span>
+        </button>
+      </form>
+    </li>
     <li style="padding: 0 1rem 0 1rem;"><Darkmode/></li>
   </ul>
 </nav>
@@ -25,5 +35,20 @@
     height: 2rem;
     border-radius: 100%;
     object-fit: cover;
+  }
+  form {
+    display: flex;
+    align-items: center;
+    gap: 0.3rem;
+    margin: 0;
+    button {
+      display: flex;
+      justify-items: center;
+      align-items: center;
+      transform: translate(-3.5rem);
+      width: min-content;
+      font-size: 0.7rem;
+      border-radius: 9999px;
+    }
   }
 </style>
