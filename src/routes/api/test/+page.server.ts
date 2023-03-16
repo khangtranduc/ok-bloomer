149d558479f69cdcc8bccfa87dd6db1575f1c9c9
Tran Duc Khang <79127891+khangtranduc@users.noreply.github.com>
2023-03-16 18:04:48 +0800

    added data population script

--- src/routes/api/test/+page.server.ts
+++
@@ -1,8 +0,0 @@
-import { db } from "$lib/database";
-
-export async function load(){
-    const [rows, _] = await db.execute("select * from user where utype = ?", ['admin']);
-    return {
-        rows: rows
-    }
-}
--- src/routes/api/test/+page.svelte
+++
@@ -1,5 +0,0 @@
-<script>
-    export let data;
-</script>
-
-{typeof data.rows}
