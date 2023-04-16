501e81866784afdf2805bf2ac17181b4676e7310
Tran Duc Khang <79127891+khangtranduc@users.noreply.github.com>
2023-04-16 17:57:07 +0800

    node apter sd

--- svelte.config.js
+++ svelte.config.js
@@ -6,14 +6,12 @@ import { vitePreprocess } from "@sveltejs/kit/vite";
 const config = {
   // Consult https://kit.svelte.dev/docs/integrations#preprocessors
   // for more information about preprocessors
-  preprocess: [
-    vitePreprocess(),
+  preprocess: 
     preprocess({
       scss: {
         prependData: '@use "src/variables.scss" as *;',
       },
     }),
-  ],
 
   kit: {
     adapter: node({ env: { port: process.env.PORT } })
