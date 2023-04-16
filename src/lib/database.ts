6b528f194a927ffd99a0027dead5c782f130bde2
Tran Duc Khang <79127891+khangtranduc@users.noreply.github.com>
2023-04-16 19:15:20 +0800

    somethings

--- src/lib/database.ts
+++ src/lib/database.ts
@@ -1,9 +1,17 @@
 import mysql from 'mysql2/promise';
 
+// export const db = mysql.createPool({
+//     host: "localhost",
+//     user: "root",
+//     password: "admin",
+//     database: "bloomer",
+//     connectionLimit: 10
+// })
+
 export const db = mysql.createPool({
-    host: "localhost",
-    user: "root",
-    password: "admin",
+    host: "35.198.210.196",
+    user: "bloomer",
+    password: "TO3bd5$tkdGP(ghQ",
     database: "bloomer",
     connectionLimit: 10
 })
